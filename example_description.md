94b99a80853cc61e493d144748cb915712199a6c
0x41gawor <41gawor@gmail.com>
2021-05-20 15:25:18 +0200

    4. Traffic Redirector

--- example_description.md
+++ example_description.md
@@ -1,11 +1,11 @@
-This extension demonstrates how to register listeners for various runtime events:
+This extension demonstrates how to redirect outgoing HTTP requests from one host to anothe.
 
-- HTTP requests and responses for all Burp tools.
-- HTTP messages intercepted by the Proxy.
-- Addition of new scan issues.
-- The extension being unloaded by the user.
 
-The sample extension simply prints a message to its output stream when an event occurs.
 
-Registering an extension state listener is particularly important for any extension that starts background threads or opens system resources (such as files or database connections). The extension should listen for itself being unloaded by the user, and should terminate any background threads or close any open resources when this event occurs. This good practice enables the user to fully unload the extension via the Burp UI.
+The extension works as follows:
+
+- It registers itself as an HTTP listener.
+- For outgoing request messages, it retrieves the HTTP service for the request.
+- If the HTTP service host matches the "from" host, it uses a helper method to build a new HTTP service using the "to" host, and other details unchanged.
+- It updates the HTTP request with the new HTTP service.
 
--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,103 +1,59 @@
 package burp;
 
-// Copied from: https://github.com/PortSwigger/example-event-listeners/blob/master/java/BurpExtender.java
-
-import java.io.PrintWriter;
-
-public class BurpExtender implements IBurpExtender,
-        // This class implements several interface so it can use methods used to listen to specific events
-        IHttpListener, IProxyListener, IScannerListener, IExtensionStateListener
+public class BurpExtender implements IBurpExtender, IHttpListener
 {
-    // We want to have reference for `callbacks` send to us as `registerExtenderCallbacks` param
-    private IBurpExtenderCallbacks callbacks;
-    // Standard output for Burp Extensions
-    private PrintWriter stdout;
+    // Change it to hosts u want to redirect between
+    private static final String HOST_FROM = "host1.example.org";
+    private static final String HOST_TO = "host2.example.org";
 
-    // How does Burp works?
-    // From our point of view Burp just generates some events and can register callbacks for this events.
-    // In our extension we have a method that can be used to register our class to receive callbacks
+    private IExtensionHelpers helpers;
+
+    //
+    // implement IBurpExtender
+    //
+
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
     {
-        // keep a reference to our callbacks object
-        // This method is an entry point for our extension, and receive callbacks object
-        // in order to acces this object in other BurpExtender methods we need a field in class
-        this.callbacks = callbacks;
+        // obtain an extension helpers object
+        helpers = callbacks.getHelpers();
+        /*
+         helpers is an `IExtensionHelpers` object that will help you:
+         - analyze HTTP Request or Response
+         - Build HTTP service (in order to put it in HTTP message)
+         - encode and decode HTTP messages
+         - Build HTTP Request or Response (HTTP message in general)
+         */
 
         // set our extension name
-        callbacks.setExtensionName("Event listeners");
-
-        // obtain our output stream
-        stdout = new PrintWriter(callbacks.getStdout(), true);
+        callbacks.setExtensionName("Traffic redirector");
 
         // register ourselves as an HTTP listener
         callbacks.registerHttpListener(this);
-
-        // register ourselves as a Proxy listener
-        callbacks.registerProxyListener(this);
-
-        // register ourselves as a Scanner listener
-        callbacks.registerScannerListener(this);
-
-        // register ourselves as an extension state listener
-        callbacks.registerExtensionStateListener(this);
     }
 
     //
     // implement IHttpListener
     //
 
-    @Override // Method from IHttpListener
+    @Override
     public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo)
-            // toolFlag - indicates which tool issued the request (Intruder, Proxy, Repeater etc.)
-            //            flags are defined in `IBurpExtenderCallbacks` interface
-            // messageIsRequest - boolean indicates if HTTP message is request or response in HTTP terms
-            // messageInfo - `IHttpRequestResponse` object that represents HTTP message
     {
-        stdout.println(
-                (messageIsRequest ? "HTTP request to " : "HTTP response from ") +
-                        messageInfo.getHttpService() +
-                        " [" + callbacks.getToolName(toolFlag) + "]");
-        // Note that we can get toolName from toolFlag using callbacks objects method
-    }
+        // only process requests
+        if (messageIsRequest)
+        {
+            // get the HTTP service for the request
+            IHttpService httpService = messageInfo.getHttpService();
 
-    //
-    // implement IProxyListener
-    //
+            // We want to check if request is from some Service, and if it is redirect it to other service
 
-    @Override // Method from IProxyListener
-    public void processProxyMessage(boolean messageIsRequest, IInterceptedProxyMessage message)
-            // messageIsRequest - boolean indicates if HTTP message is request or response in HTTP terms
-            // message - `IInterceptedProxyMessage` object that represents Intercepted HTTP message
-            //           `IInterceptedProxyMessage` interface add some info to `IHttpRequestResponse` interface
-            //            u can use IInterceptedProxyMessage.getMessageInfo to get IHttpRequestResponse object
-    {
-        stdout.println(
-                (messageIsRequest ? "Proxy request to " : "Proxy response from ") +
-                        message.getMessageInfo().getHttpService());
-    }
-
-    //
-    // implement IScannerListener
-    //
-
-    @Override // Method from IScannerListener
-    public void newScanIssue(IScanIssue issue)
-    {
-        ///TODO Still dunno what Issues is this about
-        stdout.println("New scan issue: " + issue.getIssueName());
-    }
-
-    //
-    // implement IExtensionStateListener
-    //
-
-    @Override // Method from IExtensionStateListener
-    public void extensionUnloaded()
-    {
-        // IExtensionStateListener has only one method and it is fired when user unloads the extension
-        // Here u do some cleaning up after your extensions
-        // Abort threads, clear memory used for resources, close open files etc.
-        stdout.println("Extension was unloaded");
+            // if the host is HOST_FROM, change it to HOST_TO
+            if (HOST_FROM.equalsIgnoreCase(httpService.getHost())) // equalsIgnoreCase is String's method
+            {
+                // We've got HTTPmessage in method params and we are setting a new Service for it
+                messageInfo.setHttpService(helpers.buildHttpService(HOST_TO, httpService.getPort(), httpService.getProtocol()));
+            }
+            ///TODO Do we send it further? IDK but 90% we don't in this code.
+        }
     }
 }
