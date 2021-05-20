2f705849273242a2025d893d634e21d52a22152f
0x41gawor <41gawor@gmail.com>
2021-05-20 12:16:23 +0200

    1. Empty example

---
+++ src/burp/BurpExtender.java
@@ -0,0 +1,9 @@
+package burp;
+// copied from https://portswigger.net/burp/extender/writing-your-first-burp-suite-extension#top
+public class BurpExtender implements IBurpExtender
+{
+    public void registerExtenderCallbacks (IBurpExtenderCallbacks callbacks)
+    {
+        // your extension code here
+    }
+}
