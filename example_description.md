1c3e036cbab6d7c9bff961c16cb28eee8dea12e7
0x41gawor <41gawor@gmail.com>
2021-05-21 00:35:40 +0200

    7. Custom Message Editor Tab

--- example_description.md
+++ example_description.md
@@ -1,197 +1,28 @@
-# Custom Logger
+# custom editor tab
 
-This extension provides something that has often been requested: a suite-wide HTTP logger within the main Burp UI.
+This extension demonstrates how you can extend Burp's own HTTP message editor to handle the display and editing of unsupported data formats. This capability can let you handle custom serialization implemented by specific applications, or any other data format that Burp does not natively support.
 
-This example shows how to create an UI Tab and fill it with content (in contrary to 5. example).
+In the past, some extensions have handled unsupported serialization formats by hooking into Burp's HTTP stack, and modifying incoming and outgoing messages, in order to unpack and repack the serialized data. Although this approach can work, it is quite restricted in the type of data it can handle. And it is also inelegant: it would be preferable to customize Burp to understand the custom format itself, rather than tampering with the integrity of HTTP messages.
 
-
-
-The extension uses the following techniques, which are made possible by the [extender API](https://portswigger.net/burp/extender/):
-
-- It creates a custom **tab** within the main **Burp UI**, in which to display the message log.
-- It creates two instances of Burp's own **HTTP message editor**, in which to display the selected request and response (as in the Proxy history).
-- It provides an implementation of **IMessageEditorController**, which the message editors can query to obtain additional details about the displayed messages (to support context menu actions, etc.).
-- It asks Burp to customize its own UI components, in line with Burp's UI style.
-- It adds an HTTP listener, to receive details of requests and responses made by all Burp tools.
-- It uses an extension helper method to analyze the URL in each request (`helpers.analyzeRequest(messageInfo).getUrl()`)
-
-
-
-## Overall UI preview
-
-Burp Suite with our extension tab open presents this way.
+The [extender API](https://portswigger.net/burp/extender/) lets you add custom tabs to Burp's HTTP message editor. When a message is about to be displayed, Burp will ask the tab whether it can handle the message. If so, the custom tab will be shown in the editor, and can support rendering and editing of the message within its own UI:
 
 ![](example_description_img/1.png)
 
-### Tab UI
+So basically the extension ads this option to **HTTP message editor** (which you learned in 6. Custom Logger).
 
-Burp Suite uses Swing library to build UI.
+## Base64InputTab
 
-Our Tab main node is `SplitPane` with `VERTICAL_SPLIT` layout.
+First we need to create class that implements `IMessageEditorTab` interface.
 
-`SplitPane` has two components `JScrollPane` on top and `JTabbedPane` on the bottom.
+We called it `Base64InputTab`. This class represents our custom Message Editor Tab.
 
-#### JScrollPane
+We can use `ITextEditor`. An interface provided by Burp, which extends some swing component and can be used to display and edit HTTP Message.
 
-> SWING THEORY
->
-> **JScrollPane**
->
-> `JScrollPane.JScrollPane(Component view)` 
->
-> `view` is scrollable swing component.
+We use `ITextEditor` as a field in `Base64InputTab`.
 
-In our case we pass `logsTable` object, which is an instance of `Table` class defined by us.
+How it is all implemented check the code.
 
-`Table`  extends `JTable` class 
+### IMessageEditorTabFactory
 
-> SWING THEORY
->
-> The **`JTable`** is used to display and edit regular two-dimensional tables of cells.
->
-> `public void changeSelection(int row, int col, boolean toggle, boolean extend)` 
->
-> Updates the selection models of the table, depending on the state of the two flags: toggle and extend:
->
-> - `toggle`: *false*, `extend`: *false*. Clear the previous selection and ensure the new cell is selected.
-> - `toggle`: *false*, `extend`: *true*. Extend the previous selection from the anchor to the specified cell, clearing all other selections.
-> - `toggle`: *true*, `extend`: *false*. If the specified cell is selected, deselect it. If it is not selected, select it.
-> - `toggle`: *true*, `extend`: *true*. Apply the selection state of the anchor to all cells between it and the specified cell.
->
-> **Parameters:**
->
-> `rowIndex` - affects the selection at `row
-> `columnIndex` - affects the selection at `column
->
->
-> `toggle` - see description above
->
-> `extend` - if true, extend the current selection
-
-**In short**
-
-We use `JScrollPane` and put there a `Table` object which extends `JTable` class, becasue we need to implement `public void changeSelection` method in our way.
-
-#### JTabbedPane
-
-The core of this pane is object provided by Burp Suite which is `IMessageEditor`. We can see it in Burp Suite official tabs.
-
-They look like these two guys below.
-
-<img src="example_description_img/2.png" style="zoom:75%;" />
-
-In our case we display them as tabs. Which mean you can switch tabs and show only one at a time.
-
-One `IMessageEditor` for *request* and one for *response*.
-
-U can see their field declarations here
-
-```java
-private IMessageEditor requestViewer;
-private IMessageEditor responseViewer;
-```
-
-That's how we ask Burp to initialize there variables with such objects:
-
-```java
-requestViewer = callbacks.createMessageEditor(BurpExtender.this, false);
-responseViewer = callbacks.createMessageEditor(BurpExtender.this, false);
-```
-
-We want them to be in `JTabbedPane` so we call:
-
-```java
- JTabbedPane tabs = new JTabbedPane();
- tabs.addTab("Request", requestViewer.getComponent());
- tabs.addTab("Response", responseViewer.getComponent());
-```
-
-## Implemented Interfaces
-
-Our `BurpExtender` class need to implement some interaces
-
-### IBurpExtender
-
-It's obvious. See previous examples.
-
-### ITab
-
-We need it to create a tab
-
-```java
- 	//
-    // implement ITab
-    //
-
-    @Override // Method from ITab
-    public String getTabCaption() {
-        return "Custom Logger";
-    }
-
-    @Override // Method from ITab
-    // This way we tell Burp which our graphical component should represent UI Tab
-    // Make this method to return the main swing component
-    public Component getUiComponent() {
-        return splitPane;
-    }
-```
-
-### (extend) AbstractTableModel
-
-> This abstract class provides default implementations for most of the methods in the `TableModel` interface. It takes care of the management of listeners and provides some conveniences for generating `TableModelEvents` and dispatching them to the listeners
-
-We extend these methods:
-
-- `getRowCount` and return here size of our logs list
-- `getColumnCount` we have two columns "Tool" and "URL", so we return 2.
-- `getColumnName` we distinguish column by Id and return  "Tool" or"URL"
-- `getColumnClass` Returns `Object.class` regardless of `columnIndex`. Because all columns should be the same class so you can know it blindly.
-- `getValueAt(int rowIndex, int columnIndex)` we return specific element to be displayed in UI.
-
-### IMessageEditorController
-
->This interface is used by an `IMessageEditor` to obtain details about the currently displayed message. Extensions that create instances of Burp's HTTP message editor can optionally provide an implementation of `IMessageEditorController`, which the editor will invoke when it requires further information about the current message (for example, to send it to another Burp tool). Extensions that provide custom editor tabs via an `IMessageEditorTabFactory` will receive a reference to an `IMessageEditorController` object for each tab instance they generate, which the tab can invoke if it requires further information about the current message.
-
-We implement these methods:
-
-- `getHttpService` and return current item *HTTP Service*
-- `getRequest` and return current item *Request* (because our table items are HTTP messages)
-- `getResponse`  and return current item *Request (because our table items are HTTP messages)*
-
-### IHttpListener
-
-See 3. Event Listeners example.
-
-We need to fill our table with content.
-
-In `processHttpMessage` we create `LogEntry` (class defined by us) and add it to the list. Then fire `Swing.AbstractTableModel.fireTableRowsInserted(int, int)` to notify Swing UI that table content has changed.
-
-## Extension flow
-
-Some Request is detected by `IHttpListener` and its message is processed in `processHttpMessage` method.
-
-Then it is added to `logs` list and table is updated.
-
-Also u can select specific log entry and display its `IHttpRequestResponsePersisted` in `IMessageEditorController`.
-
-***
-
-Note that in `registerExtenderCallbacks` we start a thread for UI with 
-
-```java
-SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-            ...
-            // add the custom tab to Burp's UI (so this line ACTUALLY CREATES THE TAB)
-            callbacks.addSuiteTab(BurpExtender.this);
-            ...
-            }
-```
-
-## Log Entry 
-
-We defined our class to store logs.
-
-It is called `LogEntry`.
+Our `BurpExtender` class implements this interface so it can register itself as Message Editor factory and implement `createNewInstance` method which returns object of our `Base64InputTab`.
 
--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,249 +1,149 @@
 package burp;
 
+import java.awt.Component;
 
-// In this extension we will create a tab in Burp Suite so we need some graphic library
-// Burp Suite allows us to create tabs with Swing
-import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableModel;
-import java.awt.*;
-// We use URL in LogEntry class (see line 37)
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-public class BurpExtender extends
-        // Swing class that represents an abstract TableModel, it implements `Swing.TableModel` intrarface
-        AbstractTableModel
-        implements
-        IBurpExtender,
-        // To be able to create an UI Tab by Extension we need to implement ITab interface
-        ITab,
-        // This allows our request/response viewers to obtain details about the messages being displayed
-        // use to control MessageEditor
-        IMessageEditorController,
-        // Finally we implement HTTP Listener, to have the content to be displayed in our Logger
-        IHttpListener
-    {
-    // We need this field to keep a reference for callback object send to us via `registerExtenderCallbacks` param
+public class BurpExtender implements IBurpExtender,
+        //Extensions can implement this interface and then call IBurpExtenderCallbacks.registerMessageEditorTabFactory()
+        // to register a factory for custom message editor tabs.
+        // This allows extensions to provide custom rendering or editing of HTTP messages, within Burp's own HTTP editor.
+        IMessageEditorTabFactory
+{
     private IBurpExtenderCallbacks callbacks;
-
-    // helpers object so we can use some useful IExtensionHelpers methods (`helpers.analyzeRequest(messageInfo).getUrl()`)
     private IExtensionHelpers helpers;
 
-    // Main Swing Pane for UI Tab
-    private JSplitPane splitPane;
-
-
-    // Our extension is about CustomLogs, so we define our LogEntry class
-    private static class LogEntry {
-        final int tool; // Tool from where HTTP message came
-        final IHttpRequestResponsePersisted requestResponse; // HTTP message
-        final URL url; // HTTP message URL
-
-        LogEntry(int tool, IHttpRequestResponsePersisted requestResponse, URL url) {
-            this.tool = tool;
-            this.requestResponse = requestResponse;
-            this.url = url;
-        }
-    }
-
-    // This is the list of our logs
-    private final List<LogEntry> logs = new ArrayList<LogEntry>();
-
-    // Burp has built in HTTP Message Editor.
-    // This white filed, with "Raw", "HTML", "Render" etc tabs
-    // You can use it in your extension.
-    // And IMessageEditor is the tool to do so.
-    private IMessageEditor requestViewer;
-    private IMessageEditor responseViewer;
-
-    // Current item displayed in our Logger
-    // It is an IHTttpRequestResponse object (this interface represents HTTP message)
-    private IHttpRequestResponse currentlyDisplayedItem;
-
+    //
+    // implement IBurpExtender
+    //
 
     @Override
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
-        // keep a reference to our callbacks object
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
+    {
         this.callbacks = callbacks;
 
-        // obtain an extension helpers object
         helpers = callbacks.getHelpers();
 
+        callbacks.setExtensionName("Serialized input editor");
 
-        // set our extension name
-        callbacks.setExtensionName("Custom logger");
-
-        // Create our UI Tab
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                // Main Pane
-                splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-
-                // Table of log entries
-                Table logsTable = new Table(BurpExtender.this);
-                JScrollPane scrollPane = new JScrollPane(logsTable);
-
-
-                // tabs with request/response viewers
-                JTabbedPane tabs = new JTabbedPane();
-                requestViewer = callbacks.createMessageEditor(BurpExtender.this, false);
-                responseViewer = callbacks.createMessageEditor(BurpExtender.this, false);
-                tabs.addTab("Request", requestViewer.getComponent());
-                tabs.addTab("Response", responseViewer.getComponent());
-
-                // Layout Table on the left, and MessageEditor on the right.
-                // But its vertical_split so left is up, and right is actually down.
-                splitPane.setLeftComponent(scrollPane);
-                splitPane.setRightComponent(tabs);
-
-                // customize our UI components
-                callbacks.customizeUiComponent(splitPane);
-                callbacks.customizeUiComponent(logsTable);
-                callbacks.customizeUiComponent(scrollPane);
-                callbacks.customizeUiComponent(tabs);
-
-                // add the custom tab to Burp's UI (so this line ACTUALLY CREATES THE TAB)
-                callbacks.addSuiteTab(BurpExtender.this);
-
-                // register ourselves as an HTTP listener
-                callbacks.registerHttpListener(BurpExtender.this);
-            }
-        });
+        // register ourselves as a message editor tab factory
+        callbacks.registerMessageEditorTabFactory(this);
+        // ourselves has a method that returns `IMessageEditorTab` which can be displayed as an option to choose among
+        // "raw", "pretty", "render"
     }
 
     //
-    // implement ITab
+    // implement IMessageEditorTabFactory
     //
 
-    @Override // Method from ITab
-    public String getTabCaption() {
-        return "Custom Logger";
-    }
-
-    @Override // Method from ITab
-    // This way we tell Burp which our graphical component should represent UI Tab
-    // Make this method to return the main swing component
-    public Component getUiComponent() {
-        return splitPane;
-    }
-
-    //
-    // extend AbstractTableModel
-    //
-
-    @Override // from AbstractTableModel
-    public int getRowCount() {
-        // Table is list of logs in stored in `logs` list so..
-        return logs.size();
-    }
-
-    @Override // from AbstractTableModel
-    public int getColumnCount() {
-        // We can hardcode 2, because "Tool" and "URL" from `LogEntry` will be displayed in the table.
-        // LogEntry.requestResponse will be shown below in "MessageEditor"
-        return 2;
-    }
-
-    @Override // from AbstractTableModel
-    // We need to tell Burp about column names
-    public String getColumnName(int columnIndex)
+    @Override // from IMessageEditorTabFactory
+    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable)
     {
-        switch (columnIndex)
-        {
-            case 0:
-                return "Tool";
-            case 1:
-                return "URL";
-            default:
-                return "";
-        }
+        // create a new instance of our custom editor tab
+        return new Base64InputTab(controller, editable);
+        // Below we need to define a class that implements `IMessageEditorTab`
     }
 
-    @Override // from AbstractTableModel
-    public Class<?> getColumnClass(int columnIndex)
+    //
+    // class implementing IMessageEditorTab
+    //
+
+    class Base64InputTab implements IMessageEditorTab
     {
-        return String.class;
-    }
+        private boolean editable;       // it indicates if user can edit content of the HTTP Message
+        private ITextEditor txtInput;   // This interface is used to provide extensions with an instance of Burp's raw text editor,
+                                        // for the extension to use in its own UI.
+                                        // Extensions should call `IBurpExtenderCallbacks.createTextEditor()`
+                                        // to obtain an instance of this interface.
+        private byte[] currentMessage;  // Message displayed in `txtInput` in bytes
 
-    @Override // from AbstractTableModel
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        LogEntry logEntry = logs.get(rowIndex);
-
-        switch (columnIndex)
+        // Class constructor
+        public Base64InputTab(IMessageEditorController controller, boolean editable)
         {
-            case 0:
-                return callbacks.getToolName(logEntry.tool);
-            case 1:
-                return logEntry.url.toString();
-            default:
-                return "";
-        }
-    }
+            this.editable = editable;
 
-    //
-    // implement IMessageEditorController
-    //
-
-    @Override // from IMessageEditorController
-    public IHttpService getHttpService() {
-        return currentlyDisplayedItem.getHttpService();
-    }
-
-    @Override // from IMessageEditorController
-    public byte[] getRequest() {
-        return currentlyDisplayedItem.getRequest();
-    }
-
-    @Override // from IMessageEditorController
-    public byte[] getResponse() {
-        return currentlyDisplayedItem.getResponse();
-    }
-
-    //
-    // extend JTable to handle cell selection
-    //
-
-    private class Table extends JTable {
-        public Table(TableModel tableModel) {
-            super(tableModel);
+            // create an instance of Burp's text editor, to display our deserialized data
+            txtInput = callbacks.createTextEditor(); // ask burp for TextEditor object
+            txtInput.setEditable(editable);
         }
 
-        @Override //Method from JTable
-        // Updates the selection models of the table, depending on the state of the two flags: toggle and extend.
-        // https://docs.oracle.com/javase/7/docs/api/javax/swing/JTable.html#changeSelection(int, int, boolean, boolean)
-        public void changeSelection(int row, int col, boolean toggle, boolean extend) {
-            // show the log entry for the selected row
-            LogEntry logEntry = logs.get(row);
-            requestViewer.setMessage(logEntry.requestResponse.getRequest(), true);
-            responseViewer.setMessage(logEntry.requestResponse.getResponse(), false);
-            currentlyDisplayedItem = logEntry.requestResponse;
+        //
+        // implement IMessageEditorTab
+        //
 
-            super.changeSelection(row, col, toggle, extend);
-        }
-    }
-
-    //
-    // implement IHttpListener
-    //
-    // Last step is to implement HTTP listener which will fill our `logs` list with content
-    @Override
-    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
-    // only process responses
-        if (!messageIsRequest)
+        @Override //from IMessageEditorTab
+        public String getTabCaption()
         {
-            // create a new log entry with the message details
-            synchronized(logs)
+            return "Serialized input";
+        }
+
+        @Override //from IMessageEditorTab
+        // return component representing the MessageEditorTab in our case it is txtInput,
+        // which derives from Swing.Component by implementation of Burp's ITextEditor interface
+        public Component getUiComponent()
+        {
+            return txtInput.getComponent();
+        }
+
+        @Override //from IMessageEditorTab
+        // Not every message match our desired encoding
+        // Remember we define our MessageEditorTab to encode data encoded in some specific, unique format
+        public boolean isEnabled(byte[] content, boolean isRequest)
+        {
+            // enable this tab for requests containing a data parameter
+            //return isRequest && null != helpers.getRequestParameter(content, "data");
+            return isRequest;
+            // Here we just simply return true if HTTP message is request
+        }
+
+        @Override //from IMessageEditorTab
+        public void setMessage(byte[] content, boolean isRequest)
+        {
+            if (content == null)
             {
-                int row = logs.size();
-                // Our LogEntry class contructor as 2nd param take IHttpRequestResponsePersisted object
-                // And this is the way `callbacks.saveBuffersToTempFiles(messageInfo)` we obtain it from IHttpRequestResponse object
-                logs.add(new LogEntry(toolFlag, callbacks.saveBuffersToTempFiles(messageInfo), helpers.analyzeRequest(messageInfo).getUrl()));
-                // Swing.Table method that notifies all listeners, that there were inserted a new rows in the given range
-                fireTableRowsInserted(row, row);
+                // clear our display
+                txtInput.setText(null);
+                txtInput.setEditable(false);
             }
+            else
+            {
+                // retrieve the data parameter
+                IParameter parameter = helpers.getRequestParameter(content, "Host");
+
+                // deserialize the parameter value
+                // from original: // txtInput.setText(helpers.base64Decode(helpers.urlDecode(parameter.getValue())));
+                txtInput.setText(helpers.stringToBytes("Niby tam jakies serialized data"));
+                txtInput.setEditable(editable);
+            }
+
+            // remember the displayed content
+            currentMessage = content;
+        }
+
+        @Override //from IMessageEditorTab
+        public byte[] getMessage()
+        {
+            // determine whether the user modified the deserialized data
+            if (txtInput.isTextModified())
+            {
+                // reserialize the data
+                byte[] text = txtInput.getText();
+                String input = helpers.urlEncode(helpers.base64Encode(text));
+
+                // update the request with the new parameter value
+                return helpers.updateParameter(currentMessage, helpers.buildParameter("Host", input, IParameter.PARAM_BODY));
+            }
+            else return currentMessage;
+        }
+
+        @Override //from IMessageEditorTab
+        public boolean isModified()
+        {
+            return txtInput.isTextModified();
+        }
+
+        @Override //from IMessageEditorTab
+        public byte[] getSelectedData()
+        {
+            return txtInput.getSelectedText();
         }
     }
 }
