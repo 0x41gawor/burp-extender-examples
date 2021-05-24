da4547603d10b7263b72523ffff86896bbc1f4d4
0x41gawor <41gawor@gmail.com>
2021-05-24 20:16:00 +0200

    8. Custom Scan Insertion Points

--- example_description.md
+++ example_description.md
@@ -1,28 +1,38 @@
-# custom editor tab
+# Custom Scan Insertion Points
 
-This extension demonstrates how you can extend Burp's own HTTP message editor to handle the display and editing of unsupported data formats. This capability can let you handle custom serialization implemented by specific applications, or any other data format that Burp does not natively support.
+In the `7. Custom Message Editor Tab` we saw how extension can be used to render and edit a custom HTTP message data format with Burp UI. For the purpose of demonstrating this capability, we used a trivial serialization format, in which user-supplied input is Base64-encoded within a request parameter value.
 
-In the past, some extensions have handled unsupported serialization formats by hooking into Burp's HTTP stack, and modifying incoming and outgoing messages, in order to unpack and repack the serialized data. Although this approach can work, it is quite restricted in the type of data it can handle. And it is also inelegant: it would be preferable to customize Burp to understand the custom format itself, rather than tampering with the integrity of HTTP messages.
 
-The [extender API](https://portswigger.net/burp/extender/) lets you add custom tabs to Burp's HTTP message editor. When a message is about to be displayed, Burp will ask the tab whether it can handle the message. If so, the custom tab will be shown in the editor, and can support rendering and editing of the message within its own UI:
+
+That example contained a rather obvious XSS vulnerability: the raw input contained within the serialized data is echoed unfiltered in the application's response. But although this type of bug might be obvious to a human, automated scanners will not (in general) identify any kinds of input-based vulnerabilities in cases where the raw input needs to be embedded within an unsupported serialization format. Since the scanner does not understand the format, it has no means of submitting its usual **scan payloads*** in the way that is needed for the application to unpack and process the payloads and trigger any bugs.
+
+> *Scan payloads - because that how scans work. Scanner sends tons of requests with some payloads and analyze the web page response. For example it sends some script via *HTTP request body*. Server should reject such *request*. Such traps are called **payloads**. So Scanner basically sends *HTTP requests* with some payloads and analyze the response.
+
+This means that in this situation, equipped only with the (`7.`) [example of a custom editor tab extension](https://github.com/PortSwigger/example-custom-editor-tab), you would be restricted to manual testing for input-based bugs, which is a tedious and time-consuming process.
+
+The [extender API](https://portswigger.net/burp/extender/) lets you tackle this problem by registering your extension as a provider of custom scanner insertion points. For each actively scanned request, Burp will call out to your extension, and ask it to provide any custom insertion points that are applicable to the request. Each insertion point that you provide is responsible for the job of constructing validly-formed requests for specific scan payloads. This lets your extension work with any data format, and embed the scanner's payloads within the request in the correct way.
+
+
+
+**Insertion point** is a point in *HTTP request* where our extension puts some payload.
+
+## Test it out
+
+Just run the Burp Scan and see the results. 
+
+We added our Insertion Points to default scan and see what happens. Our Insertion Point is named **"Base64-wrapped input"** and it inserts the `"4511d<script>alert(1)</script>script>ac60311e3ee"` string into *HTTP body param* named `"data"` (so it is only applied to *HTTP requests* that contain such *body param*).
+
+***
+
+Here, we can see Burp reporting the XSS vulnerability, which it has found via the custom "Base64-wrapped input" insertion point:
 
 ![](example_description_img/1.png)
 
-So basically the extension ads this option to **HTTP message editor** (which you learned in 6. Custom Logger).
+Here is the request that Burp made, and which was generated for Burp by our custom insertion point:
 
-## Base64InputTab
+![](example_description_img/2.png)
 
-First we need to create class that implements `IMessageEditorTab` interface.
+Here, via our custom message editor tab, is the literal scan payload that is embedded in the request:
 
-We called it `Base64InputTab`. This class represents our custom Message Editor Tab.
-
-We can use `ITextEditor`. An interface provided by Burp, which extends some swing component and can be used to display and edit HTTP Message.
-
-We use `ITextEditor` as a field in `Base64InputTab`.
-
-How it is all implemented check the code.
-
-### IMessageEditorTabFactory
-
-Our `BurpExtender` class implements this interface so it can register itself as Message Editor factory and implement `createNewInstance` method which returns object of our `Base64InputTab`.
+![](example_description_img/3.png)
 
--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,14 +1,15 @@
 package burp;
 
-import java.awt.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BurpExtender implements IBurpExtender,
-        //Extensions can implement this interface and then call IBurpExtenderCallbacks.registerMessageEditorTabFactory()
-        // to register a factory for custom message editor tabs.
-        // This allows extensions to provide custom rendering or editing of HTTP messages, within Burp's own HTTP editor.
-        IMessageEditorTabFactory
-{
-    private IBurpExtenderCallbacks callbacks;
+        //Extensions can implement this interface and then call `IBurpExtenderCallbacks.registerScannerInsertionPointProvider()`
+        // to register a factory for custom Scanner insertion points.
+        // Insertion points are points where Scanner puts some payloads on HTTP request
+        IScannerInsertionPointProvider {
+
     private IExtensionHelpers helpers;
 
     //
@@ -16,134 +17,100 @@ public class BurpExtender implements IBurpExtender,
     //
 
     @Override
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
-    {
-        this.callbacks = callbacks;
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
 
         helpers = callbacks.getHelpers();
+        callbacks.setExtensionName("Serialized input scan insertion point");
 
-        callbacks.setExtensionName("Serialized input editor");
+        // register ourselves as a scanner insertion point provider
+        callbacks.registerScannerInsertionPointProvider(this);
+    }
 
-        // register ourselves as a message editor tab factory
-        callbacks.registerMessageEditorTabFactory(this);
-        // ourselves has a method that returns `IMessageEditorTab` which can be displayed as an option to choose among
-        // "raw", "pretty", "render"
+    @Override //from IScannerInsertionPointProvider
+    // Burp Suite just iterates through HTTP request he can send to a webpage and asks if extension has some
+    // InsertionPoints for this HTTP request to test them out
+
+    //This one has such list if HTTP request contain 'data' param.
+    public List<IScannerInsertionPoint> getInsertionPoints(IHttpRequestResponse baseRequestResponse) {
+        // retrieve the data parameter
+        IParameter dataParameter = helpers.getRequestParameter(baseRequestResponse.getRequest(), "data");
+        if (dataParameter == null)
+            return null;
+
+        List<IScannerInsertionPoint> insertionPoints = new ArrayList<IScannerInsertionPoint>();
+        // To InsertionPoint constructor we send the whole HTTP request and its 'data' param value
+        insertionPoints.add(new InsertionPoint(baseRequestResponse.getRequest(), dataParameter.getValue()));
+        return insertionPoints;
     }
 
     //
-    // implement IMessageEditorTabFactory
+    // class implementing IScannerInsertionPoint
     //
-
-    @Override // from IMessageEditorTabFactory
-    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable)
+    private class InsertionPoint implements IScannerInsertionPoint
     {
-        // create a new instance of our custom editor tab
-        return new Base64InputTab(controller, editable);
-        // Below we need to define a class that implements `IMessageEditorTab`
-    }
+        private byte[] baseRequest;             //entry request
+                                                // we want to insert payload: e.g. "<script>alert(1)</script>"
+        private String insertionPointPrefix;    // so this is the text before payload
+        private String baseValue;               // this is the text where payload will be, just a param value
+        private String insertionPointSuffix;    // text between `baseValue` and '&' ('&' is used to separate multiple values)
 
-    //
-    // class implementing IMessageEditorTab
-    //
+        InsertionPoint(byte[] baseRequest, String dataParameterValue) {
 
-    class Base64InputTab implements IMessageEditorTab
-    {
-        private boolean editable;       // it indicates if user can edit content of the HTTP Message
-        private ITextEditor txtInput;   // This interface is used to provide extensions with an instance of Burp's raw text editor,
-                                        // for the extension to use in its own UI.
-                                        // Extensions should call `IBurpExtenderCallbacks.createTextEditor()`
-                                        // to obtain an instance of this interface.
-        private byte[] currentMessage;  // Message displayed in `txtInput` in bytes
+            this.baseRequest = baseRequest;
+            // URL- and base64-decode the data
+            dataParameterValue = helpers.bytesToString(helpers.base64Decode(helpers.urlDecode(dataParameterValue)));
 
-        // Class constructor
-        public Base64InputTab(IMessageEditorController controller, boolean editable)
-        {
-            this.editable = editable;
+            int start = dataParameterValue.indexOf("input=") + 6;
+            insertionPointPrefix = dataParameterValue.substring(0, start);
+            int end = dataParameterValue.indexOf("&", start);
+            if (end == -1) end = dataParameterValue.length();// If "&" is not found
 
-            // create an instance of Burp's text editor, to display our deserialized data
-            txtInput = callbacks.createTextEditor(); // ask burp for TextEditor object
-            txtInput.setEditable(editable);
+            baseValue = dataParameterValue.substring(start, end);
+            insertionPointSuffix = dataParameterValue.substring(end, dataParameterValue.length());
+            // In `example_description_img/3.png` variable values are:
+            // insertionPointPrefix = "input="
+            // baseValue = "4511dac60311e3ee"
+            // insertionPointSuffix = "time1355742156502"
         }
 
-        //
-        // implement IMessageEditorTab
-        //
-
-        @Override //from IMessageEditorTab
-        public String getTabCaption()
-        {
-            return "Serialized input";
+        @Override// from IScannerInsertionPoint
+        public String getInsertionPointName() {
+            return "Base64-wrapped input";
         }
 
-        @Override //from IMessageEditorTab
-        // return component representing the MessageEditorTab in our case it is txtInput,
-        // which derives from Swing.Component by implementation of Burp's ITextEditor interface
-        public Component getUiComponent()
-        {
-            return txtInput.getComponent();
+        @Override// from IScannerInsertionPoint
+        public String getBaseValue() {
+            return baseValue;
         }
 
-        @Override //from IMessageEditorTab
-        // Not every message match our desired encoding
-        // Remember we define our MessageEditorTab to encode data encoded in some specific, unique format
-        public boolean isEnabled(byte[] content, boolean isRequest)
-        {
-            // enable this tab for requests containing a data parameter
-            //return isRequest && null != helpers.getRequestParameter(content, "data");
-            return isRequest;
-            // Here we just simply return true if HTTP message is request
+        @Override // from IScannerInsertionPoint
+        // Burp Suite has some buil-in payloads to test e.g. XSS vulnerability
+        // And Burp will send here "<script>alert(1)</script>"
+        public byte[] buildRequest(byte[] payload) {
+            // build the raw data using the specified payload
+            String input = insertionPointPrefix + helpers.bytesToString(payload) + insertionPointSuffix;
+
+            // Base64- and URL-encode the data
+            input = helpers.urlEncode(helpers.base64Encode(input));
+
+            // update the request with the new parameter value
+            return helpers.updateParameter(baseRequest, helpers.buildParameter("data", input, IParameter.PARAM_BODY));
+            // `helpers.buildParameter` method doc:
+            // build a HTTP message param with given (name, value, type[body, cookie, json etc..])
         }
 
-        @Override //from IMessageEditorTab
-        public void setMessage(byte[] content, boolean isRequest)
-        {
-            if (content == null)
-            {
-                // clear our display
-                txtInput.setText(null);
-                txtInput.setEditable(false);
-            }
-            else
-            {
-                // retrieve the data parameter
-                IParameter parameter = helpers.getRequestParameter(content, "Host");
-
-                // deserialize the parameter value
-                // from original: // txtInput.setText(helpers.base64Decode(helpers.urlDecode(parameter.getValue())));
-                txtInput.setText(helpers.stringToBytes("Niby tam jakies serialized data"));
-                txtInput.setEditable(editable);
-            }
-
-            // remember the displayed content
-            currentMessage = content;
+        @Override// from IScannerInsertionPoint
+        public int[] getPayloadOffsets(byte[] payload) {
+            // since the payload is being inserted into a serialized data structure, there aren't any offsets
+            // into the request where the payload literally appears
+            return null;
         }
 
-        @Override //from IMessageEditorTab
-        public byte[] getMessage()
-        {
-            // determine whether the user modified the deserialized data
-            if (txtInput.isTextModified())
-            {
-                // reserialize the data
-                byte[] text = txtInput.getText();
-                String input = helpers.urlEncode(helpers.base64Encode(text));
-
-                // update the request with the new parameter value
-                return helpers.updateParameter(currentMessage, helpers.buildParameter("Host", input, IParameter.PARAM_BODY));
-            }
-            else return currentMessage;
-        }
-
-        @Override //from IMessageEditorTab
-        public boolean isModified()
-        {
-            return txtInput.isTextModified();
-        }
-
-        @Override //from IMessageEditorTab
-        public byte[] getSelectedData()
-        {
-            return txtInput.getSelectedText();
+        @Override// from IScannerInsertionPoint
+        public byte getInsertionPointType() {
+            return INS_EXTENSION_PROVIDED;
+            //Used to indicate where the insertion point is provided by an extension-registered IScannerInsertionPointProvider.
         }
     }
 }
+
